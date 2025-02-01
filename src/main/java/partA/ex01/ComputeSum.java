d82485e2de3ae8b1d956eb3e9675210d40f4c15f
Christian <dynexanimations21@gmail.com>
2025-02-01 11:48:14 -0500

    save
    save

--- src/main/java/partA/ex01/ComputeSum.java
+++ src/main/java/partA/ex01/ComputeSum.java
@@ -1,13 +1,32 @@
 package partA.ex01;
 
+import java.lang.invoke.VarHandle;
+import java.util.Scanner;
+import java.util.ArrayList;
 public class ComputeSum {
 
     public static String computeSum() {
         String response = "";
 
-        /**
-         * Your code goes here
-         */
+        ArrayList<Integer> num = new ArrayList<Integer>();
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Input a number (Type 0 to exit)");
+            int user = scan.nextInt();
+
+            if (user == 0) {break;}
+            num.add(user);
+        }
+
+        int total = 0;
+        for (var i = 0; i < num.size(); i++) {
+            System.out.println(num.get(i));
+            total += num.get(i);
+        }
+
+        response = "Sum = " + String.valueOf(total);
+
 
 
 
@@ -22,5 +41,5 @@ public class ComputeSum {
 
 
 
-    }
+}
 
